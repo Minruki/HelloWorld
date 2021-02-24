80a6fe7b62afe8f4a833bde8a2ddb8c4cb8f30d6
Minruki <79563714+Minruki@users.noreply.github.com>
2021-02-24 16:07:42 +0900

    Update HelloWorld.java
    추가함

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,10 +2,12 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow World");
+		System.out.println("Hello World");
 		System.out.println("World's end Dancehall");
+				
+		System.out.println("Earth's end Dancehall");
 		System.out.println("Hello end Dancehall");
-		
+		System.out.println("origin");
 		
 	}
 
